0233a5036a8dbdb1c808b90b592c1b667f6f5dac
Pieter Paßmann <pieter.passmann@gmail.com>
2015-06-03 16:49:31 +0200

    So uhm yeah
    I am trying to push this so I can use this at work.

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -1,5 +1,9 @@
 module.exports = {
     all: {
+		options: {
+			beautify: true,
+			width: 100,
+		},
         files: [{
             expand: true,
             cwd: 'html/skin/frontend/future/default/scripts/',
